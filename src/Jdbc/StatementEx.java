276cabd570efd5ce2b1376c1e00211da6f6d4b7d
sakshi <sakshibule07@gmail.com>
2025-04-21 10:59:20 +0530

    done with query

--- src/Jdbc/StatementEx.java
+++ src/Jdbc/StatementEx.java
@@ -17,9 +17,18 @@ public class StatementEx {
 		try {
 			con = DriverManager.getConnection("jdbc:mysql://localhost/morning","root","root12345");
 			
+			
 			stmt = con.createStatement();
+			
+//****************************************Creating Table query**************************************************
 			stmt.execute("CREATE TABLE Employee(id INT PRIMARY KEY, name VARCHAR(20), position VARCHAR(20))");
-			 System.out.println("Table done");
+			System.out.println("Table done");
+			
+//you have to comment above create table query to run insert data query
+			 
+//******************************************Inserting Data query in table **************************************
+			 stmt.execute("insert into employee values(101,'sakshi','java developer')");
+			 System.out.println("done with record");
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
